4d9685b7d9a3054c40283ae9df200148f0b09190
Alex Kashi <akashi@berkeley.edu>
2024-05-26 19:23:31 -0700

    Updated resume

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -5,32 +5,22 @@ const positions = [
     link: 'https://www.lucidmotors.com/',
     daterange: 'May 2022 - Present',
     points: [
-      'Led the development of the next-generation native mobile app from the ground up, introducing previously unachievable features and enhanced performance, completed ahead of schedule.',
-      'Designed a keyless entry system using Bluetooth and location APIs, enhancing reliability and user convenience.',
-      'Integrated an interactive 3D model into the app, transitioning it from concept to production, thereby boosting aesthetics and user engagement.',
-      'Worked directly with the Senior VP of Digital to ensure development efforts aligned with Lucid\'s strategic initiatives, maximizing customer impact.',
+      'Led the development of the next-generation native mobile app, introducing previously unachievable features and'
+      + 'enhanced performance, completed ahead of schedule, and raised the App Store rating by 0.2 points.',
+      'Designed a passive entry system using Bluetooth and location APIs, enhancing reliability and user convenience.',
+      'Integrated an interactive 3D model, elevating aesthetics and identified as the most liked new feature by customers.',
+      'Reported directly to the Senior VP of Digital to align development with strategic initiatives.',
     ],
   },
   {
-    company: 'Dubbles.ai',
-    position: 'Founder',
-    link: 'https://dubbles.ai/',
-    daterange: 'December 2023 - Present',
-    points: [
-      'Designed and launched an AI-powered celebrity cloning app, allowing users to create and share personalized celebrity videos.',
-      'Engineered an advanced machine learning pipeline for precise facial recognition and voice synthesis, ensuring realistic celebrity impersonations.',
-      'Led a cross-functional team through all phases of development, from initial design to deployment and scaling, achieving significant user engagement and virality.',
-    ],
-  },
-  {
-    company: 'Harvard',
+    company: 'Harvard Medical School',
     position: 'Machine Learning Research Scientist',
     link: 'https://www.rouskinlab.com/',
     daterange: 'May 2023 - Aug 2023',
     points: [
-      'Adapted protein structure prediction models to analyze RNA secondary structures, achieving state-of-the-art F1 score performance.',
-      'Curated a comprehensive database of over 300,000 secondary structures from various sources.',
-      'Developed training algorithms for synthetic data, significantly closing the generalization gap.',
+      'Adapted protein structure prediction models for RNA, achieving a state-of-the-art F1 score of 0.52 (up from 0.47).',
+      'Curated a diverse database of over 300,000 RNA secondary structures, enhancing the model training dataset.',
+      'Developed synthetic data training algorithms, significantly reducing the generalization gap between RNA types.',
     ],
   },
   {
@@ -39,11 +29,12 @@ const positions = [
     link: 'https://spurdating.com',
     daterange: 'January 2019 - December 2022',
     points: [
-      'Founded and architected a native social media app for iOS and Android, attracting over 100,000 users.',
+      'Founded and developed a native social media app for iOS and Android, reaching over 100,000 users and 350,000 downloads.',
       'Admitted into the Harvard Innovation Labs Venture Program and selected for the semester-long Harvard Business School accelerator course, Field X.',
-      'Crafted and executed a comprehensive go-to-market strategy, pitch deck, and business plan, which were presented to angel investors and venture capitalists to secure pre-seed funding.',
+      'Crafted and executed a comprehensive go-to-market strategy, pitch deck, and business plan, which'
+      + 'were presented to angel investors and venture capitalists to secure pre-seed funding.',
       'Engineered a microservice-based backend utilizing AWS technologies including DynamoDB, Elasticsearch, S3, Lambda, EC2, Kinesis, and Rekognition.',
-      'Established a complete DevOps pipeline for streamlined deployment, analytics, logging, and operational management.',
+      'Established a complete DevOps pipeline for streamlined deployment, analytics, logging and operational management.',
       'Directed a multidisciplinary team of over five professionals, overseeing projects spanning marketing, competitive analysis, and design.',
     ],
   },
@@ -53,10 +44,10 @@ const positions = [
     link: 'https://med.stanford.edu/sgtc.html',
     daterange: 'Sep 2017 - Sep 2021',
     points: [
-      'Devised the first classifier for Myalgic Encephalomyelitis (ME), published in PNAS.',
-      'Engineered cost-effective hardware and embedded software in C for an impedance measurement device, reducing costs by approximately 98%.',
-      'Authored the statistical theory underpinning the metabolic origin of ME, published in Diagnostics.',
-      'Led a team specializing in semantic segmentation of cell images, slashing computation time by 94% while improving accuracy by 2%.',
+      'Developed the first classifier for Myalgic Encephalomyelitis (ME), results published in PNAS.',
+      'Engineered hardware and software in C for an impedance measurement device, cutting costs by 98%.',
+      'Authored the statistical theory on the metabolic origin of ME, published in Diagnostics.',
+      'Led a team on cell semantic segmentation, cutting computation time by 94% and boosting accuracy by 2%.',
       'Supervised computer vision and machine learning interns, supported by the Stanford Bio-X program.',
     ],
   },
@@ -66,9 +57,8 @@ const positions = [
     link: 'https://intel.com',
     daterange: 'Jun 2017 - Sep 2017',
     points: [
-      'Enhanced drivers for I2C and UART interfaces, extending support to touch screens and cameras.',
-      'Debugged BIOS ASL code, implemented additional features, and configured new peripherals.',
-      'Developed firmware for x86-based R&D devices.',
+      'Enhanced I2C and UART drivers, extending support to touch screens and cameras.',
+      'Developed firmware for x86 R&D devices, implemented additional features, and configured new peripherals.',
     ],
   },
   {
@@ -77,9 +67,8 @@ const positions = [
     link: 'https://zSpace.com',
     daterange: 'May 2016 - Aug 2016',
     points: [
-      'Developed a gaze tracking system using deep learning and computer vision techniques, designed to be effective even when glasses obstruct the view.',
-      'Achieved all key milestones by enhancing object detection, training the machine learning model, establishing a data collection application, and developing an API for real-time gaze tracking.',
-      'Successfully launched the system within the expected timeline and validated the concept, proving that no additional hardware was necessary.',
+      'Developed a gaze tracking system using deep learning and computer vision, achieving 95% accuracy.',
+      'Curated dataset, trained model, and developed real-time API, launching on schedule without additional hardware.',
     ],
   },
   {
@@ -88,9 +77,8 @@ const positions = [
     link: 'https://intel.com',
     daterange: 'May 2015 - Aug 2015',
     points: [
-      'Designed a MapReduce framework for local big data processing on Android devices.',
-      'Parallelized the framework using pthreads in native C/C++ with the Android NDK.',
-      'Developed an application for the framework, implementing k-means clustering to preprocess data locally before transmitting results to the server.',
+      'Developed a MapReduce framework for big data processing on Android, using C/C++ for parallelization.',
+      'Applied it by implementing k-means clustering for local data preprocessing before server transmission.',
     ],
   },
 ];
