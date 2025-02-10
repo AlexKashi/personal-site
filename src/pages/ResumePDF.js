c79aec7acb477b24069bc81e606ba1e4b65440d7
Alex Kashi <akashi@berkeley.edu>
2025-02-09 19:30:50 -0800

    Update

--- src/pages/ResumePDF.js
+++ src/pages/ResumePDF.js
@@ -1,12 +1,15 @@
 // ResumePDF.js
 
 import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 function ResumePDF() {
+  const navigate = useHistory();
+
   useEffect(() => {
     // window.location.href = 'https://drive.google.com/file/d/16KmiwvlUGFqliQdUNhlCTq41e2PpekWW/view';
     window.location.replace('https://drive.google.com/file/d/16KmiwvlUGFqliQdUNhlCTq41e2PpekWW/view');
-  }, []);
+  }, [navigate]);
   return null;
 }
 
